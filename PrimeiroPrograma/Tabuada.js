26ebd0142c2240aaf99ecd57dfd9381109d6aaf9
Neander Fontes <neanderffontes@gmail.com>
2023-10-11 12:32:04 +0100

    Update Tabuada.js
    Acrescentando outro método

--- PrimeiroPrograma/Tabuada.js
+++ PrimeiroPrograma/Tabuada.js
@@ -3,10 +3,17 @@
 //Import Scanner para entrada de dados:
 const input = require("prompt-sync")();
 
-//Entrada de dados pelo utilizador:
-const numEntrada = input("Insira um valor para imprimir a tabuada do mesmo: ")
+//Entrada de dados pelo utilizador sem Ciclo while:
+// const NUMENTRADA = input("Insira um valor para imprimir a tabuada do mesmo: ")
+// for (let numIndice = 0; numIndice < 10; numIndice++) {
+//     console.log((numIndice + 1) + " x " + NUMENTRADA + " = " + ((numIndice + 1) * NUMENTRADA))
+// }
 
-//Laço para percorrer a tabuada do numero inserido:
-for (let numIndice = 0; numIndice < 10; numIndice++) {
-    console.log((numIndice + 1) + " x " + numEntrada + " = " + ((numIndice + 1) * numEntrada))
-}
+let numEntrada = null;
+do {
+    numEntrada = input("Insira um valor para imprimir a tabuada do mesmo: ")//Laço para percorrer a tabuada do numero inserido:
+    for (let numIndice = 0; numIndice < 10; numIndice++) {
+        console.log((numIndice + 1) + " x " + numEntrada + " = " + ((numIndice + 1) * numEntrada))
+    }
+} while(numEntrada > 0)
+
