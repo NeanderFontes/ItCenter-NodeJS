2e582430e5cfd1d5666b6c0c8e180b44b3fcedd3
Neander Fontes <neanderffontes@gmail.com>
2023-10-15 22:17:17 +0100

    Create main10.js
    Manipulando Elementos html e css através da DOM

---
+++ ConceitosJS/main10.js
@@ -0,0 +1,14 @@
+//Manipulando elementos do DOM:
+
+const item = document.querySelector(".items")
+//Removendo itens da li:
+//item.remove();
+
+//Removendo li específica da lista li
+//item.firstChild.remove()
+
+const btnItem = document.querySelector('.btn')
+
+console.log(btnItem)
+//Alterando style background do botão:
+btnItem.style.background = "red"
