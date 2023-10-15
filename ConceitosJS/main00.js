5d77a79913fb1f7624ab404e66aa23c5f53ab3e7
Neander Fontes <neanderffontes@gmail.com>
2023-10-15 12:03:26 +0100

    Create main00.js
    //Aprendizado:
        1- Variáveis e Tipos de Dados
        2- Lista(Arrays)
        3- JSON
        4- Loops(for, for of, while, forEach)
        5- Condicionais(if, else, switch, ternary)
        6- Funções & Arrow Funtions
        7- POO(Classes e Herança)
        8- Selecionar Elementods do DOM
        9- Manipular Elementos do DOM
        10- Eventos
        11- Criar & Validar formulários

---
+++ ConceitosJS/main00.js
@@ -0,0 +1,23 @@
+//Conceitos Básicos da linguagem JavaScript para interagir com Node.js
+
+//1º) JavaScript é um uma linguagem de programação Interpretada:
+// Ao mesmo tempo que o código é lido, o mesmo já é executado, não precisa de compilação para o código "funcionar".
+//2º) É Executado no navegador (clientside), e também utilzado no servidor do Node.js.
+//3º) É uma linguagem de programação padrão utilizado por navegadores em geral.
+
+//Aprendizado:
+/*
+    1- Variáveis e Tipos de Dados
+    2- Lista(Arrays)
+    3- JSON
+    4- Loops(for, for of, while, forEach)
+    5- Condicionais(if, else, switch, ternary)
+    6- Funções & Arrow Funtions
+    7- POO(Classes e Herança)
+    8- Selecionar Elementods do DOM
+    9- Manipular Elementos do DOM
+    10- Eventos
+    11- Criar & Validar formulários
+*/
+
+// Primeiro Passo: Sempre Importante Inicializar o JS no html para ser executado.
